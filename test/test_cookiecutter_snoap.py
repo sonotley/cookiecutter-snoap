cd8341d57c0479abb4713c20a97b293ef3686cea
Simon Notley <43099400+sonotley@users.noreply.github.com>
2022-11-15 20:18:14 +0000

    Corrected pyproject.toml

--- test/test_cookiecutter_snoap.py
+++ test/test_cookiecutter_snoap.py
@@ -1,5 +1,16 @@
 import subprocess
+import os
+import sys
 
 
 def test_cookiecutter():
-    subprocess.run(["cookiecutter", ".", "--no-input"])
+    """Test that no errors are thrown running the cookiecutter itself, also forms the setup for following tests"""
+    subprocess.run(["cookiecutter", ".", "--no-input", "-f"], check=True)
+    assert os.path.exists('my-lovely-project')
+
+
+def test_install():
+    if sys.platform == "linux":
+        subprocess.run(["sh", "my-lovely-project/dist/install_on_linux.sh"], check=True)
+    elif sys.platform == "darwin":
+        subprocess.run(["sh", "my-lovely-project/dist/install_on_linux.sh", "~"], check=True)
