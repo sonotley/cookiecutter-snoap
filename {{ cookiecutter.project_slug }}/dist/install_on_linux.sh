596cfba02a39a03aaaaaf80954f44fbc96556ed4
Simon Notley <43099400+sonotley@users.noreply.github.com>
2022-12-09 16:49:55 +0000

    Comments

--- {{ cookiecutter.project_slug }}/dist/install_on_linux.sh
+++ {{ cookiecutter.project_slug }}/dist/install_on_linux.sh
@@ -5,21 +5,32 @@ echo Installing {{ cookiecutter.project_name }}
 echo "*************************"
 filePath=${1:-"/opt"}/{{ cookiecutter.project_slug }}
 echo Installing to "$filePath"
+
+# Create and activate a Python venv
 echo Building Python virtual environment
 python3 -m venv "$filePath"/env
 target=("$(dirname "$0")"/{{ cookiecutter.package_slug }}*.whl)
 source "$filePath"/env/bin/activate
+
+# Install the pinned dependencies from requirements.txt, then install the wheel
 pip install --upgrade pip
 pip install -Ur "$(dirname "$0")"/requirements.txt || { echo Installation with pinned dependencies failed, attempting local dependency resolution; pinFail=1;}
 pip install "${target[0]}"
+
+# Create links to the binary for convenience, one at top level and one in a bin directory
 ln -s "$filePath"/env/bin/{{ cookiecutter.project_slug }} "$filePath"/{{ cookiecutter.project_slug }}
 mkdir "$filePath"/bin
 ln -s "$filePath"/env/bin/{{ cookiecutter.project_slug }} "$filePath"/bin/{{ cookiecutter.project_slug }}
+
+# Copy files from installer directory to their target locations
 {% if cookiecutter.config_file_type != "none" %}cp "$(dirname "$0")"/config.{{ cookiecutter.config_file_type }} "$filePath"/config.{{ cookiecutter.config_file_type }}{% endif %}
 cp -r "$(dirname "$0")"/data "$filePath"/data
 cp -r "$(dirname "$0")"/resources "$filePath"/resources
 cp -r "$(dirname "$0")"/readme_for_app.md "$filePath"/readme.md
+
+# Record details of installation method in a Python module accessible at run-time
 echo 'install_method, install_target = "one_dir","'"$filePath"'"' > "$filePath"/env/lib/python*/site-packages/{{ cookiecutter.package_slug }}/_options.py
+
 echo "*******************************"
 echo Installation complete
 if [[ $pinFail = 1 ]]
