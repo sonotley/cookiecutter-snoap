7d99686a4683951b7b546837f966b056810ff0aa
Simon Notley <43099400+sonotley@users.noreply.github.com>
2023-02-17 18:59:07 +0000

    Started porting recent improvements to install_on_windows.cmd

--- {{ cookiecutter.project_slug }}/dist/install_on_windows.cmd
+++ {{ cookiecutter.project_slug }}/dist/install_on_windows.cmd
@@ -2,46 +2,48 @@
 echo *************************
 echo Installing {{ cookiecutter.project_name }}
 echo *************************
-IF %1.==. set filePath=C:\{{ cookiecutter.project_slug }}& GOTO install
-set filePath=%1\{{ cookiecutter.project_slug }}
+IF %1.==. set targetParentDir=C:\& GOTO install
+set targetParentDir=%1
 
 rem Create and activate a Python venv
 :install
-echo Installing to %filePath%
+targetDir=%targetParentDir%\{{ cookiecutter.project_slug }}
+sourceDir=%~dp0
+echo Installing from %sourceDir% to %targetDir%
 echo Building Python virtual environment
-py -3 -m venv %filePath%\env --clear
-%filePath%\env\Scripts\python.exe -m pip install --upgrade pip
-call %filePath%\env\Scripts\activate
+py -3 -m venv %targetDir%\env --clear
+%targetDir%\env\Scripts\python.exe -m pip install --upgrade pip
+call %targetDir%\env\Scripts\activate
 
 rem Install the pinned dependencies from requirements.txt, then install the wheel
-FOR /F "delims=" %%i IN ('dir /b %~dp0{{ cookiecutter.package_slug }}*.whl') DO set target=%~dp0%%i
-FOR /F "delims=" %%i IN ('dir /b %~dp0requirements.txt') DO set requirements=%~dp0%%i || echo "WARNING: no requirements file found"
+FOR /F "delims=" %%i IN ('dir /b %sourceDir%{{ cookiecutter.package_slug }}*.whl') DO set target=%sourceDir%%%i
+FOR /F "delims=" %%i IN ('dir /b %sourceDir%requirements.txt') DO set requirements=%sourceDir%%%i || echo "WARNING: no requirements file found"
 set pinFail=0
-pip install -r %requirements% || set pinFail=1
-pip install %target%
+py -3 -m pip install -r %requirements% || set pinFail=1
+py -3 -m pip install %target%
 
 rem Create links to the binary for convenience, one at top level and one in a bin directory
-mklink /H %filePath%\{{ cookiecutter.project_slug }}.exe %filePath%\env\Scripts\{{ cookiecutter.project_slug }}.exe
-if not exist %filePath%\bin\ mkdir %filePath%\bin
-mklink /H %filePath%\bin\{{ cookiecutter.project_slug }}.exe %filePath%\env\Scripts\{{ cookiecutter.project_slug }}.exe
+mklink /H %targetDir%\{{ cookiecutter.project_slug }}.exe %targetDir%\env\Scripts\{{ cookiecutter.project_slug }}.exe
+if not exist %targetDir%\bin\ mkdir %targetDir%\bin
+mklink /H %targetDir%\bin\{{ cookiecutter.project_slug }}.exe %targetDir%\env\Scripts\{{ cookiecutter.project_slug }}.exe
 
 rem Copy files from installer directory to their target locations
 rem Alias robocopy with switches that prevent any overwriting and suppress output
 set "rcopy=robocopy /S /E /XC /XN /XO /NFL /NDL /NJH /NJS /nc /ns /np"
 {% if cookiecutter.config_file_type != "none" %}
-(%rcopy% %~dp0 %filePath% config.{{ cookiecutter.config_file_type }}) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
+(%rcopy% %sourceDir% %targetDir% config.{{ cookiecutter.config_file_type }}) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
 {% endif %}
-copy %~dp0readme_for_app.md %filePath%\readme.md
-if not exist %~dp0data\ mkdir %~dp0data
-if not exist %~dp0resources\ mkdir %~dp0resources
-(%rcopy% %~dp0resources %filePath%\) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
-(%rcopy% %~dp0data %filePath%\) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
+copy %sourceDir%readme_for_app.md %targetDir%\readme.md
+if not exist %sourceDir%data\ mkdir %sourceDir%data
+if not exist %sourceDir%resources\ mkdir %sourceDir%resources
+(%rcopy% %sourceDir%resources %targetDir%\) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
+(%rcopy% %sourceDir%data %targetDir%\) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
 
 rem Record details of installation method in a Python module accessible at run-time
-echo install_method, install_target = "one_dir","%filePath%" > %filePath%\env\Lib\site-packages\{{ cookiecutter.package_slug }}\_options.py
+echo install_method, install_target = "one_dir","%targetDir%" > %targetDir%\env\Lib\site-packages\{{ cookiecutter.package_slug }}\_options.py
 
 echo *************************
 echo Installation complete
 if %pinFail%==1 echo WARNING: pinned versions of dependencies could not be installed. Instead dependency resolution was performed by pip, it will probably work but is not exactly as tested.
-echo Consider adding %filePath%\bin to your PATH for quick access to {{ cookiecutter.project_name }}
+echo Consider adding %targetDir%\bin to your PATH for quick access to {{ cookiecutter.project_name }}
 echo *************************
